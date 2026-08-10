938ae861577e8fc7e9c93dc6dca6502709a23cfd
loarsaw <amanahmed1385@gmail.com>
2026-08-10 19:48:28 +0530

    remove packages directory

--- packages/core/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { VigilantClient } from "./client";
-export { VigilantApiError, VigilantNetworkError } from "./errors";
-export type {
-  Position,
-  ListPositionsParams,
-  ApplyPayload,
-  ApplyResponse,
-  VigilantClientConfig,
-} from "./types";
