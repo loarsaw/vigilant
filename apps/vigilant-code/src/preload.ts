84ef6aa41b7e8c0ab8fc8223b90f4df8c45dcad2
loarsaw <amanahmed1385@gmail.com>
2026-09-23 19:02:14 +0530

    refactor(preload): remove deep-link IPC bridge

--- apps/vigilant-code/src/preload.ts
+++ apps/vigilant-code/src/preload.ts
@@ -3,14 +3,7 @@ import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("api", {
   isDev: () => ipcRenderer.invoke("dev:isDev"),
-  onDeepLink: (callback: (data: any) => void) => {
-    ipcRenderer.on("deep-link", (_event, data) => callback(data));
-  },
-  removeDeepLinkListener: () => {
-    ipcRenderer.removeAllListeners("deep-link");
-  },
   getAllProcesses: () => ipcRenderer.invoke("get-all-processes"),
-
   setAuthToken: (token: string) => ipcRenderer.invoke("auth:setToken", token),
   getAuthToken: () => ipcRenderer.invoke("auth:getToken"),
   clearAuthToken: () => ipcRenderer.invoke("auth:clearToken"),
@@ -20,8 +13,6 @@ declare global {
   interface Window {
     api: {
       isDev: () => Promise<{ isDev: boolean }>;
-      onDeepLink: (callback: (data: any) => void) => void;
-      removeDeepLinkListener: () => void;
       getAllProcesses: () => Promise<any>;
       setAuthToken: (token: string) => Promise<void>;
       getAuthToken: () => Promise<string | null>;
